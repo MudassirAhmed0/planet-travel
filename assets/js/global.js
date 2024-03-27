2b47ac1ea6bf35b99b1012e5bdd37ebad5891e57
Mudassir Ahmed <mudassir0ahmed1@gmail.com>
2024-03-27 22:55:02 +0500

    origin

--- assets/js/global.js
+++ assets/js/global.js
@@ -9,10 +9,14 @@ document.querySelectorAll(".faq").forEach((item) => {
     const offsetHeight =
       item.parentElement.querySelector(".faq_answer p").offsetHeight;
     const tarEl = item.parentElement.querySelector(".faq_answer");
+
+    const plusIcon = item.querySelector("#plusIcon");
     if (tarEl.classList.contains("active")) {
       tarEl.style.maxHeight = 0;
+      plusIcon.classList.remove("active");
     } else {
       tarEl.style.maxHeight = offsetHeight + "px";
+      plusIcon.classList.add("active");
     }
     tarEl.classList.toggle("active");
   });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        averageMobile: "375px"
+      }
+    }
   },
   plugins: []
 };

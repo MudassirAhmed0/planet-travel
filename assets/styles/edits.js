3a6a35d14c8ee01d6a6f88479b6299fa20ad8934
Mudassir Ahmed <mudassir0ahmed1@gmail.com>
2024-03-31 03:39:01 +0500

    origin

--- assets/styles/edits.js
+++ assets/styles/edits.js
@@ -3,9 +3,21 @@ const setFormBuilderItemsHeight = () => {
     const formSidebar = document.querySelector(".form-sidebar");
     const formSidebarHeight = formSidebar.offsetHeight;
 
+    const viewportHeight = window.innerHeight;
+    const totalHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight
+    );
+    const maxScrollableHeight = totalHeight - viewportHeight - 50;
+
     const formBuilderMenu = document.querySelector(".form-builder-menu");
     const formBuilderMenuHeight = formBuilderMenu.offsetHeight;
-    const margins = window.scrollY > 100 ? 60 : 150;
+    const margins =
+      window.scrollY < 100
+        ? 150
+        : window.scrollY > maxScrollableHeight
+        ? 60
+        : 30;
     console.log(formBuilderMenuHeight);
     const formBuilderItems = document.querySelector("#form-builder-items");
     let newHeight = window.innerHeight - formBuilderMenuHeight - margins + "px";

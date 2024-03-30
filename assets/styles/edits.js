7ce4c0f8b1625560dca68868b77934bbf087f1d4
Mudassir Ahmed <mudassir0ahmed1@gmail.com>
2024-03-31 02:54:09 +0500

    origin

--- assets/styles/edits.js
+++ assets/styles/edits.js
@@ -1,17 +1,24 @@
 const setFormBuilderItemsHeight = () => {
-  if (window.innerWidth > 1065) {
-    const formSidebar = document.querySelector(".form-sidebar");
-    const formSidebarHeight = formSidebar.offsetHeight;
+  let firstVisit = true;
+  setTimeout(
+    () => {
+      if (window.innerWidth > 1065) {
+        const formSidebar = document.querySelector(".form-sidebar");
+        const formSidebarHeight = formSidebar.offsetHeight;
 
-    const formBuilderMenu = document.querySelector(".form-builder-menu");
-    const formBuilderMenuHeight = formBuilderMenu.offsetHeight;
+        const formBuilderMenu = document.querySelector(".form-builder-menu");
+        const formBuilderMenuHeight = formBuilderMenu.offsetHeight;
 
-    const formBuilderItems = document.querySelector("#form-builder-items");
-    let newHeight = formSidebarHeight - formBuilderMenuHeight - 65 + "px";
-    formBuilderItems.style.maxHeight = newHeight;
-    console.log(formBuilderItems);
-    console.log(newHeight);
-  }
+        const formBuilderItems = document.querySelector("#form-builder-items");
+        let newHeight = formSidebarHeight - formBuilderMenuHeight - 65 + "px";
+        formBuilderItems.style.maxHeight = newHeight;
+        console.log(formBuilderItems);
+        console.log(newHeight);
+      }
+      firstVisit = false;
+    },
+    firstVisit ? 1000 : 0
+  );
 };
 
 setFormBuilderItemsHeight();
